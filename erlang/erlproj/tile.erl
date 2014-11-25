ef15fb0a3b6dcec7fa7cbcf63b0334288ca182e9
Toon Nolten <toon.nolten@student.kuleuven.be>
2014-11-25 22:37:21 +0100

    tile dirlocations

--- erlang/erlproj/tile.erl
+++ erlang/erlproj/tile.erl
@@ -1,24 +1,53 @@
+%
+%   Toon Nolten
+%
 -module(tile).
 
 -export([tilemain/1]).
 
 tilemain( Id ) ->
+    glob:registerName(glob:regformat(Id), self()),
 	tilemain(Id, 0).
 
 tilemain( Id, Value ) ->
-	ok.
+    tilelife(Id, Value, 0).
 
+dirdelta(up) -> -4;
+dirdelta(dn) ->  4;
+dirdelta(lx) -> -1;
+dirdelta(rx) ->  1.
 
-%%%%%%%%%%%%%%%%%
-% fill this out %
-%%%%%%%%%%%%%%%%%
-tilelife()->
+inbound(_, Val, up) ->
+    Val > 0;
+inbound(_, Val, dn) ->
+    Val < 17;
+inbound(Id, Val, lx) ->
+    Val > 1 + ((Id - 1) div 4) * 4 - 1;
+inbound(Id, Val, rx) ->
+    Val < 4 + ((Id - 1) div 4) * 4 + 1.
+
+dirlocations(Id, Direction) ->
+    Delta = dirdelta(Direction),
+    Inbound = inbound(Id, Id + Delta, Direction),
+    if
+        Inbound ->
+            [Id | dirlocations(Id + dirdelta(Direction), Direction)];
+        true ->
+            [Id]
+    end.
+
+furthest(Value, Furthest, [H | T]) -> 
+    
+furthest(_, Furthest, []) ->
+    Furthest.
+
+tilelife(Id, CurrentValue, Merged)->
 	receive
 		die ->
 			debug:debug("I, ~p, die.~n",[Id]),
 			exit(killed);
 		up ->
-			ok;
+			dirlocations(Id, up);
 		dn ->
 			ok;
 		lx ->
@@ -26,8 +55,8 @@ tilelife()->
 		rx ->
 			ok;
 		{yourValue, Repl} ->
-			Repl ! {tilevalue, Id, CurrentValue, Merged};
+			Repl ! {tilevalue, Id, CurrentValue, Merged},
+            tilelife(Id, CurrentValue, Merged);
 		{setvalue, Future, NewMerged} ->
-			ok
-	end,
-	tilelife( ).
+			tilelife(Id, Future, NewMerged)
+	end.
