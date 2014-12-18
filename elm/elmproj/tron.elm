134ea294b4573eb09d22454b17089d978ce28edb
Toon Nolten <toon.nolten@student.kuleuven.be>
2014-12-18 20:07:57 +0100

    een begin van blink

--- elm/elmproj/tron.elm
+++ elm/elmproj/tron.elm
@@ -1,36 +1,58 @@
 import Color
+import Text
 
--- default values
-width = 1024
-height = 768
-
-playerW = 64
-playerH = 16
+-- default values, units are pixels
+text   = { blink = 50 } -- percentage
+field  = { width = 1024,
+           height = 768,
+           bgcolor = black }
+player = { w = 64,
+           h = 16 }
+tail   = { width = 2,
+           length = 500 }
 
 data Orientation = N | E | S | W
+
+background = constant (filled field.bgcolor (rect field.width field.height))
+
+hud = lift2 (\ b t -> if b then t else "")
+            blink
+            (constant (toForm (centered (Text.color white
+                                            (toText "Not playing")))))
+
+blink = foldp (\ c l -> xor c l)
+              True
+              (sampleOn (every (100*millisecond)) (constant True))
+
+showGrid : Signal Element
+showGrid = let forms = [background, hud]
+            in collage (truncate field.width)
+                       (truncate field.height)
+                       forms
+
 -- example use: showPlayer' Color.lightBlue (10, 10) N
 showPlayer' : Color -> (Float, Float) -> Orientation -> Form
 showPlayer' color (x, y) o =
-    let fw = toFloat playerW
+    let fw = toFloat player.w
         (xOffset, yOffset, degs) = case o of
                     N -> (0, fw / 2, 90)
                     E -> (fw / 2, 0, 0)
                     S -> (0, -fw / 2, -90)
                     W -> (-fw / 2, 0, 180)
-    in rect (fw) (toFloat playerH) 
+    in rect (fw) (toFloat player.h) 
         |> outlined (solid color)
         |> move (x, y)
         |> move (xOffset, yOffset)
         |> rotate (degrees degs) 
 
-tailWidth = 2
 
 -- example use: showTail Color.lightBlue [(10, 10), (20, 10)]
 showTail : Color -> [(Float, Float)] -> Form
 showTail color positions = 
     traced { defaultLine |
-        width <- tailWidth
+        width <- tail.width
         , color <- color
         } (path positions)
 
-main = constant <| plainText "implement me"
+main = showGrid
+
