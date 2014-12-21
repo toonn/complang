2d6ae8810c473f6f27cd3fdcb62ca17b608717b7
Toon Nolten <toon.nolten@student.kuleuven.be>
2014-12-21 18:05:59 +0100

    blinking text

--- elm/elmproj/tron.elm
+++ elm/elmproj/tron.elm
@@ -2,7 +2,8 @@ import Color
 import Text
 
 -- default values, units are pixels
-text   = { blink = 50 } -- percentage
+text   = { tRatio = 50, -- percentage
+           freq = 0.2 }
 field  = { width = 1024,
            height = 768,
            bgcolor = black }
@@ -13,22 +14,41 @@ tail   = { width = 2,
 
 data Orientation = N | E | S | W
 
-background = constant (filled field.bgcolor (rect field.width field.height))
+-- This signal emits a bool freq times per second.
+-- This bool is tRatio% of the time true, false otherwise.
+blinkTF : Float -> Float -> Signal Bool
+blinkTF tRatio freq = foldp (xor)
+                            True
+                            (sampleOn (every (1000/freq*millisecond))
+                                      (constant True))
 
-hud = lift2 (\ b t -> if b then t else "")
-            blink
-            (constant (toForm (centered (Text.color white
-                                            (toText "Not playing")))))
+-- This function converts a string into a blinking string.
+blink : String -> Signal String
+blink string = keepWhen (blinkTF text.tRatio text.freq)
+                      ""
+                      (constant string)
 
-blink = foldp (\ c l -> xor c l)
-              True
-              (sampleOn (every (100*millisecond)) (constant True))
 
+-- The background of the "grid" a plain black rectangle of fixed size.
+background : Signal [Form] -> Signal [Form]
+background = lift ((::) (filled field.bgcolor <| rect field.width field.height))
+
+-- The hud displays the status (in text) of the game.
+hud : Signal [Form] -> Signal [Form]
+hud = lift2 (::) (lift (toForm << centered << Text.color white << toText)
+                       (blink "Not Playing"))
+
+
+-- The grid is the combination of all elements in the game that should be
+-- displayed.
+grid : Signal [Form]
+grid = background << hud <| constant []
+
+-- This signal turns the forms in the grid into an html element that can be
+-- displayed.
 showGrid : Signal Element
-showGrid = let forms = [background, hud]
-            in collage (truncate field.width)
-                       (truncate field.height)
-                       forms
+showGrid = lift (collage (truncate field.width) (truncate field.height))
+                grid
 
 -- example use: showPlayer' Color.lightBlue (10, 10) N
 showPlayer' : Color -> (Float, Float) -> Orientation -> Form
