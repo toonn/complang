5b13ff7202a7a6e056ce81d69d972ea271f0b15a
Toon Nolten <toon.nolten@student.kuleuven.be>
2014-12-22 20:06:44 +0100

    blinking text perfected, includes ratio; gamephase signal

--- elm/elmproj/tron.elm
+++ elm/elmproj/tron.elm
@@ -1,9 +1,11 @@
 import Color
 import Text
+import Keyboard
+import Debug
 
 -- default values, units are pixels
-text   = { tRatio = 50, -- percentage
-           freq = 0.2 }
+text   = { tRatio = 66, -- percentage
+           freq = 0.5 }
 field  = { width = 1024,
            height = 768,
            bgcolor = black }
@@ -13,31 +15,61 @@ tail   = { width = 2,
            length = 500 }
 
 data Orientation = N | E | S | W
+data GamePhase = Pre | Mid | Post
 
--- This signal emits a bool freq times per second.
+-- This signal emits a bool freq/10 times per millisecond.
 -- This bool is tRatio% of the time true, false otherwise.
-blinkTF : Float -> Float -> Signal Bool
-blinkTF tRatio freq = foldp (xor)
-                            True
-                            (sampleOn (every (1000/freq*millisecond))
-                                      (constant True))
+blinkTF : Int -> Float -> Signal Bool
+blinkTF tRatio freq = let period = (1000/freq)*millisecond
+                       in merge (sampleOn (every period)
+                                           (constant True))
+                                 (delay ((toFloat tRatio)/100*period)
+                                        (sampleOn (every period)
+                                                  (constant False)))
 
 -- This function converts a string into a blinking string.
 blink : String -> Signal String
-blink string = keepWhen (blinkTF text.tRatio text.freq)
-                      ""
-                      (constant string)
+blink string = lift (\ bTF -> if bTF then string else "")
+                    (blinkTF text.tRatio text.freq)
+
+
+-- This signal emits Pre, Mid or Post according to which phase of the game
+-- starts.
+phase : Signal GamePhase
+phase = let spacePress = keepIf identity False Keyboard.space
+         in foldp (\ sp prev -> case prev of
+                                Pre -> Mid
+                                Mid -> Pre)
+              Pre
+              spacePress
 
 
 -- The background of the "grid" a plain black rectangle of fixed size.
 background : Signal [Form] -> Signal [Form]
 background = lift ((::) (filled field.bgcolor <| rect field.width field.height))
 
+
+-- Entry text signal.
+entry : Signal Form
+entry = (lift (toForm << centered << Text.color white << toText)
+                       (blink "Space to Enter\nThe Grid"))
+
+-- Score display signal.
+score : Signal Form
+score = (lift (toForm << centered << Text.color white << toText)
+                       (blink "score"))
+
 -- The hud displays the status (in text) of the game.
 hud : Signal [Form] -> Signal [Form]
-hud = lift2 (::) (lift (toForm << centered << Text.color white << toText)
-                       (blink "Not Playing"))
-
+hud = lift2 (::) ((\ ph e c s -> case ph of
+                                    Pre -> e
+                                    Mid -> c
+                                    Post -> s)
+                    <~ phase
+                     ~ entry
+                     ~ (lift (toForm << centered << Text.color white << toText) (blink
+                     "mid"))
+                     ~ score)
 
 -- The grid is the combination of all elements in the game that should be
 -- displayed.
