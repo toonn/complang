bfd87bd648c38703cbed6091e23fc39d79ef788e
Toon Nolten <toon.nolten@student.kuleuven.be>
2014-12-23 21:51:17 +0100

    lightcycles and jetwalls; no collision yet

--- elm/elmproj/tron.elm
+++ elm/elmproj/tron.elm
@@ -4,20 +4,28 @@ import Keyboard
 import Debug
 
 -- default values, units are pixels
-text   = { tRatio = 66, -- percentage
-           freq = 0.5 }
-field  = { width = 1024,
-           height = 768,
-           bgcolor = black }
-player = { w = 64,
-           h = 16 }
-tail   = { width = 2,
-           length = 500 }
+text       = { tRatio = 66, -- percentage
+               freq = 0.5 }
+field      = { width = 1024,
+               height = 768,
+               bgcolor = black }
+lightcycle = { w = 64,
+               h = 16,
+               velocity = 150 } -- pixels per second
+jetwall    = { width = 2,
+               length = 500 }
+fpers      = 30
 
 data Orientation = N | E | S | W
-data GamePhase = Pre | Mid | Post
 
--- This signal emits a bool freq/10 times per millisecond.
+data GamePhase = Pre | Mid | Post
+type Place = (Float, Float) -- (x, y)
+type Controls = (Keyboard.KeyCode, Keyboard.KeyCode) -- (left, right)
+data Player = Player Color Place Orientation Controls
+data Turn = Left | Right
+type Velocity = Float
+
+-- This signal emits a bool freq times per second.
 -- This bool is tRatio% of the time true, false otherwise.
 blinkTF : Int -> Float -> Signal Bool
 blinkTF tRatio freq = let period = (1000/freq)*millisecond
@@ -57,7 +65,7 @@ entry = (lift (toForm << centered << Text.color white << toText)
 -- Score display signal.
 score : Signal Form
 score = (lift (toForm << centered << Text.color white << toText)
-                       (blink "score"))
+                       (blink "__ has won!"))
 
 -- The hud displays the status (in text) of the game.
 hud : Signal [Form] -> Signal [Form]
@@ -67,14 +75,108 @@ hud = lift2 (::) ((\ ph e c s -> case ph of
                                     Post -> s)
                     <~ phase
                      ~ entry
-                     ~ (lift (toForm << centered << Text.color white << toText) (blink
-                     "mid"))
+                     ~ (lift (toForm << centered << Text.color white << toText) (blink "mid"))
                      ~ score)
+
+
+-- Calculate a new orientation from an orientation and a turn.
+turn : Turn -> Orientation -> Orientation
+turn t o = case t of
+            Left  ->
+                case o of
+                    N -> W
+                    W -> S
+                    S -> E
+                    E -> N
+            Right ->
+                case o of
+                    N -> E
+                    E -> S
+                    S -> W
+                    W -> N
+
+-- Signal of turns corresponding to keypresses from the controls.
+turns : Controls -> Signal Turn
+turns (leftKey, rightKey) =
+    let keyToTurns k t = sampleOn (keepIf (identity) False
+                                    <| Keyboard.isDown k)
+                                  (constant t)
+     in merge (keyToTurns leftKey Left) (keyToTurns rightKey Right)
+
+-- This signal represents the direction something is facing, which changes
+-- with the controls.
+directions : Orientation -> Signal Turn -> Signal Orientation
+directions initialDirection = foldp (turn) initialDirection
+
+-- Calculate a new place from a velocity (px/s), a place and an orientation.
+mv : Velocity -> Orientation -> Place -> Place
+mv v o (x, y) = let distance = v/(1000/25)
+                   in case o of
+                        N -> (x,            y + distance)
+                        E -> (x + distance, y)
+                        S -> (x,            y - distance)
+                        W -> (x - distance, y)
+
+-- This signal represents a position that changes with a certain velocity
+-- in a certain direction.
+places : Place -> Signal Orientation -> Signal Place
+places startPlace directions =
+    foldp (mv lightcycle.velocity)
+          startPlace
+          (sampleOn (every (25*millisecond)) directions)
+
+-- Signal of list representations of jetwalls (lightcycle tails).
+trace : Float -> Place -> Signal Orientation -> Signal Place -> Signal [Place]
+trace length initialPlace dirs plcs =
+    let crop len xs =
+            case foldl (\ (x, y) (ps, (px, py), l) ->
+                    let distance = (abs (x-px)) + (abs (y-py))
+                     in if l + distance <= len
+                            then (ps ++ [(x,y)], (x,y), l + distance)
+                            else let np =
+                                    if | y == py && x < px -> (px - (len-l), py)
+                                       | y == py && x > px -> (px + (len-l), py)
+                                       | y < py -> (px, py - (len-l))
+                                       | otherwise -> (px, py + (len-l))
+                                  in (ps ++ [np], np, len))
+                 ([], (head xs), 0)
+                 xs of
+                (ps, _, _) -> ps
+     in fst <~ foldp (\ (dir, plc) (trc, lastdir) ->
+                     (crop jetwall.length
+                           (if dir == lastdir
+                                then plc :: (drop 1 trc)
+                                else plc :: trc),
+                      dir))
+                 ([initialPlace], W)
+                 ((,) <~ dirs ~ plcs)
+
+-- This signal represents a player.
+player : Player -> [Signal Form]
+player (Player color place orientation controls) =
+    let dirs = directions orientation (turns controls)
+        plcs = places place dirs
+        sample signal = sampleOn (fps fpers) signal
+     in ((showPlayer' color)
+            <~ sample plcs
+             ~ dirs)
+        :: ((showTail color)
+            <~ sample (trace jetwall.length place dirs plcs))
+        :: []
+
+-- This signal represents all players in the game.
+players : [Player] -> Signal [Form] -> Signal [Form]
+players ps = lift2 (++) (combine <| concat <| map player ps)
 
 -- The grid is the combination of all elements in the game that should be
 -- displayed.
 grid : Signal [Form]
-grid = background << hud <| constant []
+grid = background
+        << hud
+        << players [Player white (100,100) N (37,39),
+                    Player green (50, 100) N (37,39),
+                    Player orange (0,100) N (37, 39)]
+        <| constant []
 
 -- This signal turns the forms in the grid into an html element that can be
 -- displayed.
@@ -85,13 +187,13 @@ showGrid = lift (collage (truncate field.width) (truncate field.height))
 -- example use: showPlayer' Color.lightBlue (10, 10) N
 showPlayer' : Color -> (Float, Float) -> Orientation -> Form
 showPlayer' color (x, y) o =
-    let fw = toFloat player.w
+    let fw = toFloat lightcycle.w
         (xOffset, yOffset, degs) = case o of
                     N -> (0, fw / 2, 90)
                     E -> (fw / 2, 0, 0)
                     S -> (0, -fw / 2, -90)
                     W -> (-fw / 2, 0, 180)
-    in rect (fw) (toFloat player.h) 
+    in rect (fw) (toFloat lightcycle.h) 
         |> outlined (solid color)
         |> move (x, y)
         |> move (xOffset, yOffset)
@@ -102,7 +204,7 @@ showPlayer' color (x, y) o =
 showTail : Color -> [(Float, Float)] -> Form
 showTail color positions = 
     traced { defaultLine |
-        width <- tail.width
+        width <- jetwall.width
         , color <- color
         } (path positions)
 
